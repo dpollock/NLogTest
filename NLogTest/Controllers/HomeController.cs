ef8cab2c4f1bc99621d8c05890dae14877be2aeb
Daniel Pollock <daniellpollock@gmail.com>
2016-10-14 14:44:34 +0530

    Added Nlog example

--- NLogTest/Controllers/HomeController.cs
+++ NLogTest/Controllers/HomeController.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLog;
 
 namespace NLogTest.Controllers
 {
     public class HomeController : Controller
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public ActionResult Index()
         {
+            logger.Info("This is the first line of home/index method.");
+
+            try
+            {
+                throw new Exception("This is one badass exception. :)");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "It seems the exception happened. :(");
+            }
+
+            logger.Warn("This is your last warning!");
+            logger.Fatal("And this is fatal error...");
+
             return View();
         }
 
